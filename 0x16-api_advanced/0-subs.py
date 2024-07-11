e7a9461b717c87d5f5d416314f072603a0fbf814
Gerger-Creative <gerges.zechariah@gmail.com>
2024-07-11 13:21:40 -0700

    0-subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,10 +10,10 @@ def number_of_subscribers(subreddit):
     returns the number of subscribers
     """
     domain = 'https://www.reddit.com'
-    path = '/r/{}/about.json'.format(subreddit)
+    path = f'/r/{subreddit}/about.json'
     url = '{}{}'.format(domain, path)
     header = {
-        'user-agent': 'linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)',
+        'user-agent': 'request',
         'over18': 'yes'
     }
     response = requests.get(
@@ -24,6 +24,7 @@ def number_of_subscribers(subreddit):
     code = response.status_code
     if code >= 300:
         return 0
+
     data = response.json().get('data')
     subscribers = data.get('subscribers')
     return subscribers
