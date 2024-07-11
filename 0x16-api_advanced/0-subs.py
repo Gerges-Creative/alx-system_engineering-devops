da970fe578ca00df7a1125311ed61cd9fec61f1b
Gerger-Creative <gerges.zechariah@gmail.com>
2024-07-11 10:28:35 -0700

    Style

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python3
-"""
-number of subscribers for a given subreddit
-"""
+"""Module to query Reddit API for the number of subscribers in a subreddit."""
 
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
 
-    if subreddit is None or not isinstance(subreddit, str):
+    """Queries the Reddit API and returns the number of subscribers."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "My-User-Agent"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        print("Error: Received status code {}".format(response.status_code))
         return 0
 
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    results = response.json()
-
     try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
+        data = response.json().get("data")
+        if data:
+            return data.get("subscribers", 0)
+        else:
+            print("Error: 'data' field is missing in the response.")
+            return 0
+    except ValueError:
+        print("Error: Unable to parse JSON response.")
         return 0
