971f1265806ad2368f948a5c9519022a96420cee
Gerges-Creative <gerges.zechariah@gmail.com>
2024-07-12 08:31:07 -0700

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,28 +9,21 @@ def number_of_subscribers(subreddit):
     """
     returns the number of subscribers
     """
-    domain = f'https://www.reddit.com'
-    path = f'/r/{subreddit}/about.json'
-    url = f'{domain}{path}'
-    headers = {
-        'User-Agent': 'MyRedditApp/0.1 by Gerges_Z'
+    domain = 'https://www.reddit.com'
+    path = '/r/{}/about.json'.format(subreddit)
+    url = '{}{}'.format(domain, path)
+    header = {
+        'user-agent': 'Gerges_Z',
+        'over18': 'yes'
     }
-
-    try:
-        response = requests.get(
-            url,
-            headers=headers,
-            allow_redirects=False
-        )
-        # Raises HTTPError for bad responses (4xx and 5xx)
-        response.raise_for_status()
-
-        data = response.json().get('data')
-        if data:
-            subscribers = data.get('subscribers')
-            return subscribers
-        else:
-            return 0
-
-    except requests.exceptions.RequestException as e:
+    response = requests.get(
+        url,
+        headers=header,
+        allow_redirects=False
+    )
+    code = response.status_code
+    if code >= 300:
         return 0
+    data = response.json().get('data')
+    subscribers = data.get('subscribers')
+    return subscribers
